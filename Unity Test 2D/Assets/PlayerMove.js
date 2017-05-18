a7816da59a61e9c5683e3ad2679bf130d0604a11
Dox3 <nousernameavailablexcepthisone@gmail.com>
2017-05-17 18:29:44 -0700

    Made all text relative to screen sizes

--- Unity Test 2D/Assets/PlayerMove.js
+++ Unity Test 2D/Assets/PlayerMove.js
@@ -17,15 +17,15 @@
      {
         transform.Translate(transform.up * (5 * playerSpeed) * Time.deltaTime);
      }
-     if(Input.GetKey("down") && transform.position.y > -5)//Press up arrow key to move forward on the Y AXIS
+     if(Input.GetKey("down") && transform.position.y > -4.5)//Press up arrow key to move forward on the Y AXIS
      {
         transform.Translate(transform.up * (-5 * playerSpeed) * Time.deltaTime);
      }
-     if(Input.GetKey("right") && transform.position.x < 11)//Press up arrow key to move forward on the Y AXIS
+     if(Input.GetKey("right") && transform.position.x < 12)//Press up arrow key to move forward on the Y AXIS
      {
         transform.Translate(transform.right * (5 * playerSpeed) * Time.deltaTime);
      }
-     if(Input.GetKey("left") && transform.position.x > -11)//Press up arrow key to move forward on the Y AXIS
+     if(Input.GetKey("left") && transform.position.x > -13)//Press up arrow key to move forward on the Y AXIS
      {
         transform.Translate(transform.right * (5 * -playerSpeed) * Time.deltaTime);
      }
